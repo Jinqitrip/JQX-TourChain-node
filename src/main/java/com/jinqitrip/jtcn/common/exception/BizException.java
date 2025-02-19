d7cd6b5a6468249de440505ccb5873394116dbdf
fQwQf <supertjz123@foxmail.com>
2025-02-19 15:16:38 +0800

    fix: 添加方法

--- src/main/java/com/jinqitrip/jtcn/common/exception/BizException.java
+++ src/main/java/com/jinqitrip/jtcn/common/exception/BizException.java
@@ -1,10 +1,16 @@
 package com.jinqitrip.jtcn.common.exception;
 
+import lombok.RequiredArgsConstructor;
+
 public class BizException extends RuntimeException {
     private final int code;
     
     public BizException(int code, String message) {
         super(message);
         this.code = code;
+    }
+
+    public int getCode() {
+        return code;
     }
 }
